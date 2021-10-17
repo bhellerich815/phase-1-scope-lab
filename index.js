a7067ad3536fa7ea12f051b58bb56934cfdf8ede
Brooke Hellerich <brookeh815@cox.net>
2021-10-16 19:17:10 -0500

    commit tree f827e90527179e583d1f9ae6c4379a8a916b1155

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   return customerName
+};
+
+
+var bestCustomer = undefined;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer
+};
+
+
+const leastFavoriteCustomer = 'Jane';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'John';
+    return leastFavoriteCustomer
+};
+
+
+
+
+
